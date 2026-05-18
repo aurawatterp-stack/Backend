a82d0b4513b3536ed67245657f70d03db3cd031a
Priyansh <priyansh@kali.kali>
2026-05-18 17:48:32 +0530

    new

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// Vercel Node Function entrypoint.
-// Uses the compiled output (`dist/`) so runtime doesn't depend on TS/ESM resolution.
-
-const app = require("../dist/src/app.js").default;
-
-module.exports = (req, res) => app(req, res);
-
--- dist/api/index.js
+++ dist/api/index.js
@@ -3,8 +3,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = handler;
 const app_1 = __importDefault(require("../src/app"));
-function handler(req, res) {
-    return (0, app_1.default)(req, res);
-}
+exports.default = app_1.default;
